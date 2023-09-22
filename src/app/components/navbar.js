1307ec661ed8bb0997c7da6d623e80a39b00e294
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-22 15:56:42 +0300

    let see

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -5,20 +5,19 @@ import {
     Stack,
     Box,
     Text,
-    Button
+    Button,
 } from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons';
 import Link from "next/link";
 const Navbar = () => {
-    return ( 
-        <Stack direction={'row'} bg={'blue.400'} p={'20px'} h={['100px', '100px', '200px', '200px']}>
-            <Box className="Heading" w={'75%'} pt={['', '', '60px', '60px']}>
-            <Text textAlign={'center'} color={'white'} m={'auto'} fontWeight={'bold'} fontSize={['', '', '30px', '30px']}><Link href={'../gallery'}>Movie Poster Image Gallery</Link></Text>
-            </Box>
-            <Box w={'25%'} pt={['', '', '60px', '60px']}>
+    return (
+        <Stack direction={'row'}>
+            <Box> <HamburgerIcon /> </Box>
+            <Box w={'25%'}>
                 <Link href={'../'}><Button>Logout</Button></Link>
             </Box>
         </Stack>
-     );
+    );
 }
- 
+
 export default Navbar;
