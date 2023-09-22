726bd06c976d3e6af81995e7ff8246534127a153
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-22 16:26:17 +0300

    ???

--- src/app/gallery/provider.js
+++ src/app/gallery/provider.js
@@ -25,7 +25,7 @@ export function Providers({ children }) {
             <Box>
               <Stack direction={'row'}>
               <Text fontWeight={'bold'} color={'white'} fontSize={['20px', '20px', '30px', '30px']}> Hi MENTOR </Text>
-              <Avatar size={['sm', 'md', 'lg', 'xl']}>
+              <Avatar size={'md'}>
                 <AvatarBadge boxSize='1.25em' bg='green.500' />
               </Avatar>
               </Stack>
