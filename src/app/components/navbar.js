1a28b7678a2453d4c883a296255215f932648db8
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-22 15:28:40 +0300

    nav position

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -11,10 +11,10 @@ import Link from "next/link";
 const Navbar = () => {
     return ( 
         <Stack direction={'row'} bg={'blue.400'} p={'20px'} h={['100px', '100px', '200px', '200px']}>
-            <Box className="Heading" w={'75%'}>
+            <Box className="Heading" w={'75%'} pt={['', '', '90px', '90px']}>
             <Text textAlign={'center'} color={'white'} m={'auto'} fontWeight={'bold'} fontSize={['', '', '30px', '30px']}><Link href={'../gallery'}>Movie Poster Image Gallery</Link></Text>
             </Box>
-            <Box w={'25%'}>
+            <Box w={'25%'} pt={['', '', '90px', '90px']}>
                 <Link href={'../'}><Button>Logout</Button></Link>
             </Box>
         </Stack>
