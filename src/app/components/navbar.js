00411ce7cdd910ba9750bd5457b1f742b9a4e9d0
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-22 16:32:02 +0300

    lets see

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -6,13 +6,18 @@ import {
     Box,
     Text,
     Button,
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
+import { Avatar, AvatarBadge, AvatarGroup } from '@chakra-ui/react';
 import { HamburgerIcon } from '@chakra-ui/icons';
 import Link from "next/link";
 const Navbar = () => {
     return (
         <Stack direction={'row'} p={'20px'}>
-            <Box w={'33%'}> <HamburgerIcon color={'white'} /> </Box>
+            <Box w={'33%'}>
+                <Avatar size={['sm', 'md', 'md', 'md']}>
+                    <AvatarBadge boxSize='1.25em' bg='green.500' />
+                </Avatar> 
+            </Box>
             <Box w={'33%'}><Text textAlign={'center'} color={'white'}>Movie Image Gallery</Text></Box>
             <Box w={'33%'}>
                 <Link href={'../'}><Button float={'right'}>Logout</Button></Link>
